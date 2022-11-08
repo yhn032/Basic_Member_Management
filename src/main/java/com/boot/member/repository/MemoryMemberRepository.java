2c9fc5748ddcf38cd27a41e29c624b79b3e568e7
yhn032 <yhn0485761@gmail.com>
2022-11-08 17:04:49 +0900

    Member Repository and TestCase

---
+++ src/main/java/com/boot/member/repository/MemoryMemberRepository.java
@@ -0,0 +1,40 @@
+package com.boot.member.repository;
+
+import com.boot.member.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)) //파라미터로 넘어온 네임과 저장된 네임이 같은 경우만 필터링이 된다.
+                .findAny();//반드시 하나는 찾음
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
