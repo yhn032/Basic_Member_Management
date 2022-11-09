89ef957f770cb094245cc66b5755f1c4365f7cdd
yhn032 <yhn0485761@gmail.com>
2022-11-09 15:20:35 +0900

    past JDBC not using now

--- src/main/java/com/boot/member/controller/MemberController.java
+++ src/main/java/com/boot/member/controller/MemberController.java
@@ -2,6 +2,13 @@ package com.boot.member.controller;
 
 import com.boot.member.domain.Member;
 import com.boot.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
